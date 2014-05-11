0beb2a6bf76cfa91b2543867820e1807d71cf6b1
Flo <thefloff@gmx.at>
2014-05-11 15:08:53 +0200

    reanmed ids DSA-31

--- src/main/java/sepm/dsa/gui/MainMenuController.java
+++ src/main/java/sepm/dsa/gui/MainMenuController.java
@@ -2,7 +2,7 @@ package sepm.dsa.gui;
 
 import javafx.fxml.Initializable;
 
-public class MainMenueController implements Initializable {
+public class MainMenuController implements Initializable {
 
     @Override
     public void initialize (java.net.URL location, java.util.ResourceBundle resources) {
