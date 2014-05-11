278f345d756acf76b02d0aae42942d7fb9e10e34
Flo <thefloff@gmx.at>
2014-05-11 16:24:45 +0200

    RegionListController empty
    DSA-31
    DSA-56
    DSA-57

--- src/main/java/sepm/dsa/gui/regionlistController.java
+++ src/main/java/sepm/dsa/gui/regionlistController.java
@@ -1,11 +1,40 @@
 package sepm.dsa.gui;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 public class RegionListController implements Initializable {
+
+    @FXML
+    private TableView regionTable;
+    @FXML
+    private TableColumn regionColumn;
+    @FXML
+    private TableColumn borderColumn;
+    @FXML
+    private TableColumn colorColumn;
+    @FXML
+    private Button createButton;
+    @FXML
+    private Button editButton;
+    @FXML
+    private Button deleteButton;
+
 
     @Override
     public void initialize (java.net.URL location, java.util.ResourceBundle resources) {
 
     }
+
+    @FXML
+    private void onCreateButtonPressed() {}
+
+    @FXML
+    private void onEditButtonPressed() {}
+
+    @FXML
+    private void onDeleteButtonPressed() {}
 }
