3592c30b672f347a16a58b8c6aed4fea9930b307
Flo <thefloff@gmx.at>
2014-05-11 14:47:58 +0200

    Adding empty controllers

---
+++ src/main/java/sepm/dsa/gui/editregionController.java
@@ -0,0 +1,11 @@
+package sepm.dsa.gui;
+
+import javafx.fxml.Initializable;
+
+public class editregionController implements Initializable {
+
+    @Override
+    public void initialize (java.net.URL location, java.util.ResourceBundle resources) {
+
+    }
+}
