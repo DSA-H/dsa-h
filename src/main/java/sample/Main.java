4648971f74043b51a5bfecdf01ab2ca786d37f93
Martin Prebio <mp@25th-floor.com>
2014-05-11 13:43:24 +0200

    Adding a small test for the service stuff

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -5,6 +5,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import sepm.dsa.model.Region;
+import sepm.dsa.service.RegionService;
 
 public class Main extends Application {
 
@@ -18,6 +22,18 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
-        launch(args);
+	    ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+	    RegionService regionService = (RegionService) ctx.getBean("regionService");
+
+	    System.out.println("Region ID 1:" + regionService.get(1));
+
+	    Region r = new Region();
+	    r.setName("Region 1");
+	    r.setColor("424242");
+	    int id = regionService.add(r);
+	    System.out.println("Created: "+id);
+	    System.out.println(r.getName());
+
+	    System.out.println("From DB:" + regionService.get(r.getId()));
     }
 }
