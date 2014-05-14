d507dc074d36dfaab3b4c6c4d30d059b317ed68e
Michael Hoeller <e1225059@student.tuwien.ac.at>
2014-05-14 23:24:52 +0200

    jetzt endlich?

--- src/test/java/sepm/dsa/dao/test/RegionDaoTests.java
+++ src/test/java/sepm/dsa/dao/test/RegionDaoTests.java
@@ -7,12 +7,16 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import sepm.dsa.dao.RegionDao;
+import sepm.dsa.model.RainfallChance;
 import sepm.dsa.model.Region;
+import sepm.dsa.model.Temperature;
 
 /**
  * Created by Michael on 13.05.2014.
@@ -20,18 +24,15 @@ import sepm.dsa.model.Region;
 @RunWith(JUnit4.class)
 //@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
 //        DbUnitTestExecutionListener.class })
+@ContextConfiguration(locations = {"classpath:testContext.xml"})
 public class RegionDaoTests {
 
+    @Autowired
     private RegionDao regionDao;
 
 
     @Before
     public void setUp() {
-        ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
-        regionDao = (RegionDao) ctx.getBean("regionDao");
-        if (regionDao == null) {
-            throw new IllegalStateException("regionDao could not be fetched");
-        }
     }
 
     @After
@@ -47,6 +48,8 @@ public class RegionDaoTests {
         region.setName("Region1");
         region.setColor("65A3EF");
         region.setComment("comment");
+        region.setRainfallChance(RainfallChance.MONSUN);
+        region.setTemperature(Temperature.LOW);
         int id = regionDao.add(region);
         Region persistedRegion = regionDao.get(id);
         TestCase.assertTrue(persistedRegion != null);
