0f3052dce21c0cff4a1d24e9802413a4f397b2cd
Christian <chris.schlick@gmail.com>
2014-05-14 23:26:53 +0200

    TestSuite - all Tests implemented

--- src/test/java/sepm/dsa/TestSuiteAll.java
+++ src/test/java/sepm/dsa/TestSuiteAll.java
@@ -5,13 +5,14 @@ import junit.framework.TestSuite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
+import sepm.dsa.dao.test.RegionDaoTests;
 import sepm.dsa.service.test.*;
 
 /**
  * Created by Chris on 13.05.2014.
  */
 @RunWith(Suite.class)
-@SuiteClasses({ RegionServiceTest.class, RegionBorderServiceTest.class })
+@SuiteClasses({ RegionServiceTest.class, RegionBorderServiceTest.class, RegionServiceTests.class, RegionDaoTests.class })
 public class TestSuiteAll{
 
 }
