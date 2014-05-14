3350922a6f148a8dd6a0a64f2b60617f773b7ec2
Michael Hoeller <e1225059@student.tuwien.ac.at>
2014-05-14 17:14:35 +0200

    added logging to RegionDaoHbmImpl

--- src/main/java/sepm/dsa/dao/RegionDaoHbmImpl.java
+++ src/main/java/sepm/dsa/dao/RegionDaoHbmImpl.java
@@ -1,6 +1,8 @@
 package sepm.dsa.dao;
 
 import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import sepm.dsa.model.Region;
@@ -11,48 +13,60 @@ import java.util.Vector;
 @Repository
 public class RegionDaoHbmImpl implements RegionDao {
 
+    private static final Logger log = LoggerFactory.getLogger(RegionDaoHbmImpl.class);
+
     private SessionFactory sessionFactory;
 
     @Override
     public int add(Region region) {
+        log.debug("calling add(" + region + ")");
         sessionFactory.getCurrentSession().save(region);
-        return region.getId();
+        int result = region.getId();
+        log.trace("returning " + result);
+        return result;
     }
 
     @Override
     public void update(Region region) {
+        log.debug("calling update(" + region + ")");
         sessionFactory.getCurrentSession().update(region);
     }
 
     @Override
     public void remove(Region region) {
+        log.debug("calling remove(" + region + ")");
         sessionFactory.getCurrentSession().delete(region);
     }
 
     @Override
     public Region get(int id) {
+        log.debug("calling get(" + id + ")");
+
         Object result = sessionFactory.getCurrentSession().get(Region.class, id);
 
         if (result == null) {
             return null;
         }
-
+        log.trace("returning " + result);
         return (Region) result;
     }
 
     @Override
     public List<Region> getAll() {
+        log.debug("calling getAll()");
         List<?> list = sessionFactory.getCurrentSession().getNamedQuery("Region.findAll").list();
 
-        List<Region> regions = new Vector<>(list.size());
+        List<Region> result = new Vector<>(list.size());
         for (Object o : list) {
-            regions.add((Region) o);
+            result.add((Region) o);
         }
 
-        return regions;
+        log.trace("returning " + result);
+        return result;
     }
 
     public void setSessionFactory(SessionFactory sessionFactory) {
+        log.debug("calling setSessionFactory(" + sessionFactory + ")");
         this.sessionFactory = sessionFactory;
     }
 }
