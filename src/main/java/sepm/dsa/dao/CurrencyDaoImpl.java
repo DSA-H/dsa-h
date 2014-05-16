6ad49b0c5ea243f226e04a13f40e0ffec3a365bb
Georg Heiler <georg.kf.heiler@gmail.com>
2014-05-16 22:00:13 +0200

    some javadoc + interfaces

---
+++ src/main/java/sepm/dsa/dao/CurrencyDaoImpl.java
@@ -0,0 +1,35 @@
+package sepm.dsa.dao;
+
+import sepm.dsa.model.Currency;
+
+import java.util.List;
+
+/**
+ * Created by georg on 16.05.14 for project sepmDSA.
+ */
+public class CurrencyDaoImpl implements CurrencyDao{
+    @Override
+    public void add(Currency currency) {
+
+    }
+
+    @Override
+    public void update(Currency currency) {
+
+    }
+
+    @Override
+    public void remove(Currency currency) {
+
+    }
+
+    @Override
+    public Currency get(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Currency> getAll() {
+        return null;
+    }
+}
