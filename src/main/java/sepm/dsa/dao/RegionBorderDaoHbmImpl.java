bb22689296bb8f10c006ed7d1c57690ef37cff3e
Georg Heiler <georg.kf.heiler@gmail.com>
2014-05-16 19:03:24 +0200

    added logging 01

--- src/main/java/sepm/dsa/dao/RegionBorderDaoHbmImpl.java
+++ src/main/java/sepm/dsa/dao/RegionBorderDaoHbmImpl.java
@@ -1,7 +1,8 @@
 package sepm.dsa.dao;
 
 import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import sepm.dsa.model.RegionBorder;
@@ -18,50 +19,58 @@ import java.util.Vector;
 @Transactional(readOnly = true)
 public class RegionBorderDaoHbmImpl implements RegionBorderDao, Serializable {
 
+    private static final Logger log = LoggerFactory.getLogger(RegionBorderDaoHbmImpl.class);
     private SessionFactory sessionFactory;
 
     @Override
     @Transactional(readOnly = false)
     public RegionBorderPk add(RegionBorder regionBorder) {
+        log.debug("calling add(" + regionBorder + ")");
         sessionFactory.getCurrentSession().save(regionBorder);
+        log.trace("returning " + regionBorder.getPk());
         return regionBorder.getPk();
     }
 
     @Override
     @Transactional(readOnly = false)
     public void update(RegionBorder regionBorder) {
+        log.debug("calling update(" + regionBorder + ")");
         sessionFactory.getCurrentSession().update(regionBorder);
     }
 
     @Override
     @Transactional(readOnly = false)
     public void remove(RegionBorder regionBorder) {
+        log.debug("calling remove(" + regionBorder + ")");
         sessionFactory.getCurrentSession().delete(regionBorder);
     }
 
     @Override
     public RegionBorder get(RegionBorderPk pk) {
+        log.debug("calling get(" + pk + ")");
         Object result = sessionFactory.getCurrentSession().get(RegionBorder.class, pk);
         if (result == null) {
             return null;
         }
+        log.trace("returning " + result);
         return (RegionBorder) result;
     }
 
     @Override
     public List<RegionBorder> getAll() {
+        log.debug("calling getAll()");
         List<?> list = sessionFactory.getCurrentSession().getNamedQuery("RegionBorder.findAll").list();
-
         List<RegionBorder> result = new Vector<>(list.size());
         for (Object o : list) {
             result.add((RegionBorder) o);
         }
-
+        log.trace("returning " + result);
         return result;
     }
 
     @Override
     public List<RegionBorder> getAllForRegion(int regionId) {
+        log.debug("calling getAllForRegion(" + regionId + ")");
         List<?> list = sessionFactory.getCurrentSession().getNamedQuery("RegionBorder.findAllForRegion")
                 .setParameter("regionId", regionId)
                 .list();
@@ -70,11 +79,12 @@ public class RegionBorderDaoHbmImpl implements RegionBorderDao, Serializable {
         for (Object o : list) {
             result.add((RegionBorder) o);
         }
-
+        log.trace("returning " + result);
         return result;
     }
 
     public void setSessionFactory(SessionFactory sessionFactory) {
+        log.debug("calling setSessionFactory");
         this.sessionFactory = sessionFactory;
     }
 }
