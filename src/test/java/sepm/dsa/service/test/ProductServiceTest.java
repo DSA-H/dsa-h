4570c167265a40c32a60b74de7fc7f5701aa5ca7
Christian <chris.schlick@gmail.com>
2014-05-17 21:19:45 +0200

    not finished version with service interface & first spring/hibernate config

--- src/test/java/sepm/dsa/service/test/ProductServiceTest.java
+++ src/test/java/sepm/dsa/service/test/ProductServiceTest.java
@@ -1,22 +1,44 @@
 package sepm.dsa.service.test;
 
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.TransactionDbUnitTestExecutionListener;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import sepm.dsa.dao.RegionDao;
+import sepm.dsa.model.RainfallChance;
+import sepm.dsa.model.Region;
+import sepm.dsa.model.Temperature;
+import static org.junit.Assert.assertTrue;
 import sepm.dsa.service.ProductService;
 
 /**
  * Created by Chris on 17.05.2014.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:testContext.xml"})
+@TestExecutionListeners({
+        DependencyInjectionTestExecutionListener.class,
+        DbUnitTestExecutionListener.class,
+        DirtiesContextTestExecutionListener.class,
+        TransactionDbUnitTestExecutionListener.class
+})
 public class ProductServiceTest {
 
     @Autowired
     private ProductService ps;
-    //
+
     @Test
-    @DatabaseSetup("productData.xml")
+    @DatabaseSetup("/productData.xml")
     public void testXML()
     {
         System.out.println(ps.getAll().toString());
     }
+
 }
