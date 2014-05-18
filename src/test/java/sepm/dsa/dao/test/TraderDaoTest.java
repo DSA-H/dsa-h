461a517b286a2d8c447a87232375aa3c02e207fa
Johannes Wawerda <joh.wawerda@gmx.at>
2014-05-18 15:13:45 +0200

    traderDao Test DSA-64

--- src/test/java/sepm/dsa/dao/test/TraderDaoTest.java
+++ src/test/java/sepm/dsa/dao/test/TraderDaoTest.java
@@ -38,7 +38,6 @@ public class TraderDaoTest {
     public void add_shouldPersistEntity() {
         Trader trader = new Trader();
         trader.setName("TestTrader1");
-        trader.setCategory();
         trader.setCh(10);
         trader.setIn(11);
         trader.setMu(14);
@@ -66,6 +65,63 @@ public class TraderDaoTest {
         assertTrue(persistedTrader != null);
 
         traderDao.remove(trader);
+    }
+
+    @Test
+    @DatabaseSetup("/testData.xml")
+    public void add_incompleteShouldNotPersist() {
+        Trader trader = new Trader();
+        trader.setName("TestTrader1");
+        trader.setCh(10);
+        trader.setIn(11);
+        trader.setMu(14);
+        trader.setComment("test12345 Kommentar");
+        trader.setConvince(15);
+
+        traderDao.add(trader);
+
+        Trader persistedTrader = traderDao.get(trader.getId());
+        assertTrue(persistedTrader == null);
+    }
+
+    @Test
+    @DatabaseSetup("/testData.xml")
+    public void update_ShouldPersistEntity() {
+        Trader persistedTrader = traderDao.get(2);
+
+        persistedTrader.setComment("xyz123");
+
+        traderDao.update(persistedTrader);
+
+        persistedTrader = traderDao.get(2);
+
+        assertTrue(persistedTrader != null);
+        assertTrue(persistedTrader.getName().equals("Megatron"));
+        assertTrue(persistedTrader.getComment().equals("xyz123"));
+    }
+
+    @Test
+    @DatabaseSetup("/testData.xml")
+    public void update_TraderWithOffersShouldPersistEntity() {
+        Trader persistedTrader = traderDao.get(1);
+
+        Offer o1 = new Offer();
+        o1.setAmount(10);
+        o1.setPricePerUnit(170);
+        Product p1 = new Product();
+        p1.setName("testproduct");
+        p1.setAttribute(ProductAttribute.NORMAL);
+        p1.setCost(140);
+        p1.setQuality(true);
+        o1.setProduct(p1);
+        o1.setQuality(ProductQuality.SCHLECHT);
+        persistedTrader.getOffers().add(o1);
+
+        traderDao.update(persistedTrader);
+
+        persistedTrader = traderDao.get(1);
+        assertTrue(persistedTrader != null);
+        assertTrue(persistedTrader.getOffers().contains(o1));
     }
 
 }
