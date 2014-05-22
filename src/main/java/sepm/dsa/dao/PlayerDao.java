0e4de900ad0d22098006c926284b4c4eb8362b03
Martin Prebio <mp@25th-floor.com>
2014-05-22 15:27:01 +0200

    Adding player model and dao with tests and all the hibernate and spring configuration stuff
    DSA-71

---
+++ src/main/java/sepm/dsa/dao/PlayerDao.java
@@ -0,0 +1,40 @@
+package sepm.dsa.dao;
+
+import sepm.dsa.exceptions.DSAModelNotFoundException;
+import sepm.dsa.model.Player;
+
+import java.util.List;
+
+public interface PlayerDao {
+
+	/**
+	 * Adds the player to the database.
+	 * @param player to be persisted must not be null
+	 */
+	void add(Player player);
+
+	/**
+	 * Updates the given player.
+	 * @param player Player to be updated. Must not be null.
+	 */
+	void update(Player player);
+
+	/**
+	 * Removes the given player from the database.
+	 * @param player Player to be removed. Must no be null.
+	 */
+	void remove(Player player);
+
+	/**
+	 * Retrieves a player from the database.
+	 * @param id Identifier of the player.
+	 * @return the requested player or null if it can not be found
+	 */
+	Player get(int id);
+
+	/**
+	 * Retrieves a list of all players from the database.
+	 * @return list of all players
+	 */
+	List<Player> getAll();
+}
---
+++ src/test/java/sepm/dsa/dao/test/PlayerDaoTest.java
@@ -0,0 +1,81 @@
+package sepm.dsa.dao.test;
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.TransactionDbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import sepm.dsa.dao.LocationDao;
+import sepm.dsa.dao.PlayerDao;
+import sepm.dsa.model.Player;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:testContext.xml"})
+@TestExecutionListeners({
+	DependencyInjectionTestExecutionListener.class,
+	DbUnitTestExecutionListener.class,
+	DirtiesContextTestExecutionListener.class,
+	TransactionDbUnitTestExecutionListener.class
+})
+@DatabaseSetup("/testData.xml")
+public class PlayerDaoTest {
+
+	@Autowired
+	private PlayerDao playerDao;
+
+	@Autowired
+	private LocationDao locationDao;
+
+	@Test
+	public void testAddShouldPersistEntity() throws Exception {
+		Player player = new Player();
+		player.setName("Test Player");
+		player.setComment("Comment");
+		playerDao.add(player);
+
+		Player persistedPlayer = playerDao.get(player.getId());
+		assertNotNull("Expected player to be persisted", persistedPlayer);
+	}
+
+	@Test
+	public void testUpdate() throws Exception {
+		String newPlayerName = "Test Subject 1";
+
+		Player player = playerDao.get(1);
+		player.setName(newPlayerName);
+		playerDao.update(player);
+
+		assertEquals("Expected player name to change", newPlayerName, playerDao.get(1).getName());
+	}
+
+	@Test
+	public void testRemove() throws Exception {
+		playerDao.remove(playerDao.get(1));
+
+		playerDao.getAll().
+			forEach(player -> assertNotSame("Expected to be rid of the player", 1, player.getId()));
+	}
+
+	@Test
+	public void testGet() throws Exception {
+		Player player = playerDao.get(1);
+		assertNotNull("Expected a player but got null", player);
+		assertEquals("Expected the retrieving ID", new Integer(1), player.getId());
+	}
+
+	@Test
+	public void testGetAllReturnsTheCorrectAmountOfPlayers() throws Exception {
+		List<Player> players = playerDao.getAll();
+		assertEquals("Expected that size matches fixture count", 2, players.size());
+	}
+}
