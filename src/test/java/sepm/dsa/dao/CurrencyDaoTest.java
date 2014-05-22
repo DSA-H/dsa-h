2671df1d83214766bb1b2f668127f77eaed05af9
Georg Heiler <georg.kf.heiler@gmail.com>
2014-05-22 13:10:42 +0200

    still strange behavior in spring

--- src/test/java/sepm/dsa/dao/CurrencyDaoTest.java
+++ src/test/java/sepm/dsa/dao/CurrencyDaoTest.java
@@ -12,6 +12,7 @@ import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import sepm.dsa.model.Currency;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:testContext.xml"})
@@ -27,20 +28,26 @@ public class CurrencyDaoTest extends TestCase {
     private CurrencyDao currencyDao;
 
     @Test
-    @DatabaseSetup("/testCurrency.xml")
     public void testAdd() throws Exception {
+        Currency c1 = new Currency();
+        c1.setName("fofods");
+        c1.setValueToBaseRate(1);
 
+        currencyDao.add(c1);
+
+        assertNotNull(c1.getId());
     }
 
     @Test
     @DatabaseSetup("/testCurrency.xml")
     public void testRemove() throws Exception {
-
+        currencyDao.remove(currencyDao.get(1));
+        assertNull(currencyDao.get(1));
     }
 
     @Test
     @DatabaseSetup("/testCurrency.xml")
     public void testGetAll() throws Exception {
-
+        assertEquals(currencyDao.getAll().size(), 2);
     }
 }
--- src/test/java/sepm/dsa/service/CurrencyServiceTest.java
+++ src/test/java/sepm/dsa/service/CurrencyServiceTest.java
@@ -1,26 +1,76 @@
 package sepm.dsa.service;
 
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.TransactionDbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
 import junit.framework.TestCase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import sepm.dsa.dao.CurrencyDao;
+import sepm.dsa.model.Currency;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:testContext.xml"})
+@TestExecutionListeners({
+        DependencyInjectionTestExecutionListener.class,
+        DbUnitTestExecutionListener.class,
+        DirtiesContextTestExecutionListener.class,
+        TransactionDbUnitTestExecutionListener.class
+})
 public class CurrencyServiceTest extends TestCase {
 
+    @Autowired
+    private CurrencyDao currencyDao;
+
+    @Test
+    @DatabaseSetup("/testCurrency.xml")
     public void testGet() throws Exception {
-
+        assertNotNull(currencyDao.get(1));
+        assert (false);
+        //TODO mock mockito
     }
 
+    @Test
+    @DatabaseSetup("/testCurrency.xml")
     public void testAdd() throws Exception {
+        Currency c1 = new Currency();
+        c1.setName("fofods");
+        c1.setValueToBaseRate(1);
 
+        currencyDao.add(c1);
+
+        assertNotNull(c1.getId());
+        assert (false);
     }
 
+    @Test
+    @DatabaseSetup("/testCurrency.xml")
     public void testRemove() throws Exception {
-
+        currencyDao.remove(currencyDao.get(1));
+        assertNull(currencyDao.get(1));
+        assert (false);
     }
 
+    @Test
+    @DatabaseSetup("/testCurrency.xml")
     public void testGetAll() throws Exception {
-
+        assertEquals(currencyDao.getAll().size(), 2);
     }
 
+    @Test
+    @DatabaseSetup("/testCurrency.xml")
     public void testExchange() throws Exception {
 
+        Currency c1 = currencyDao.get(1);
+        Currency c2 = currencyDao.get(2);
+
+        //TODO Absprechen was passieren soll
+        assert (false);
     }
 }
