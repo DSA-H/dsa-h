d438735b28f0fb2b8cc5c7b012cef886a74f87e5
Georg Heiler <georg.kf.heiler@gmail.com>
2014-05-25 15:17:10 +0200

    all services now with javadoc

--- src/main/java/sepm/dsa/service/TraderCategoryService.java
+++ src/main/java/sepm/dsa/service/TraderCategoryService.java
@@ -1,14 +1,9 @@
 package sepm.dsa.service;
 
-import sepm.dsa.model.Location;
-import sepm.dsa.model.Trader;
 import sepm.dsa.model.TraderCategory;
 
 import java.util.List;
 
-/**
- * Created by Jotschi on 18.05.2014.
- */
 public interface TraderCategoryService {
 
     TraderCategory get(int id);
