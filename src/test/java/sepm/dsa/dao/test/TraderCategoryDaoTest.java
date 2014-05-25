b6dfd22a05bfb5bc6ec562c31ac3b8aa74c7d845
Michael Hoeller <e1225059@student.tuwien.ac.at>
2014-05-25 19:44:55 +0200

    important test case

--- src/test/java/sepm/dsa/dao/test/TraderCategoryDaoTest.java
+++ src/test/java/sepm/dsa/dao/test/TraderCategoryDaoTest.java
@@ -4,16 +4,23 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import sepm.dsa.dao.TraderCategoryDao;
 import sepm.dsa.dbunit.AbstractDatabaseTest;
+import sepm.dsa.model.AssortmentNature;
+import sepm.dsa.model.ProductCategory;
 import sepm.dsa.model.TraderCategory;
+import sepm.dsa.service.ProductCategoryService;
 
 import java.util.List;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 public class TraderCategoryDaoTest extends AbstractDatabaseTest {
 
     @Autowired
     private TraderCategoryDao traderCategoryDao;
+
+    @Autowired
+    private ProductCategoryService productCategoryService;
 
     @Test
     public void testAdd() throws Exception {
@@ -46,4 +53,20 @@ public class TraderCategoryDaoTest extends AbstractDatabaseTest {
         List<TraderCategory> l1 = traderCategoryDao.getAll();
         assertTrue(l1.contains(cat1));
     }
+
+    @Test
+    public void add_withAssortmentNatures_ShouldCascadeAddAssortments() {
+        TraderCategory cat = new TraderCategory();
+        cat.setName("Kat 1");
+        AssortmentNature a1 = new AssortmentNature();
+        a1.setDefaultOccurence(50);
+        ProductCategory pc = productCategoryService.get(1);
+        a1.setProductCategory(pc);
+        a1.setTraderCategory(cat);
+        cat.getAssortments().put(pc, a1);
+        cat = traderCategoryDao.get(cat.getId());
+        assertNotNull(cat);
+        assertTrue(cat.getAssortments().size() == 1);
+    }
+
 }
