29077c40ad982d070aa5a66762cdfcdb63594300
Martin Prebio <mp@25th-floor.com>
2014-05-25 17:52:25 +0200

    Also refactoring Currency tests to use the new AbstractDatabaseTest

--- src/test/java/sepm/dsa/dao/CurrencyDaoTest.java
+++ src/test/java/sepm/dsa/dao/CurrencyDaoTest.java
@@ -1,30 +1,17 @@
 package sepm.dsa.dao;
 
-import com.github.springtestdbunit.DbUnitTestExecutionListener;
-import com.github.springtestdbunit.TransactionDbUnitTestExecutionListener;
-import com.github.springtestdbunit.annotation.DatabaseSetup;
-import junit.framework.TestCase;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestExecutionListeners;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
-import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import sepm.dsa.dbunit.AbstractDatabaseTest;
 import sepm.dsa.model.Currency;
 
 import java.math.BigDecimal;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:testContext.xml"})
-@TestExecutionListeners({
-        DependencyInjectionTestExecutionListener.class,
-        DbUnitTestExecutionListener.class,
-        DirtiesContextTestExecutionListener.class,
-        TransactionDbUnitTestExecutionListener.class
-})
-public class CurrencyDaoTest extends TestCase {
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNull;
+import static org.junit.Assert.assertNotNull;
+
+public class CurrencyDaoTest extends AbstractDatabaseTest {
 
     @Autowired
     private CurrencyDao currencyDao;
@@ -41,14 +28,12 @@ public class CurrencyDaoTest extends TestCase {
     }
 
     @Test
-    @DatabaseSetup("/testCurrency.xml")
     public void testRemove() throws Exception {
         currencyDao.remove(currencyDao.get(1));
         assertNull(currencyDao.get(1));
     }
 
     @Test
-    @DatabaseSetup("/testCurrency.xml")
     public void testGetAll() throws Exception {
         assertEquals(currencyDao.getAll().size(), 2);
     }
--- src/test/java/sepm/dsa/service/CurrencyServiceTest.java
+++ src/test/java/sepm/dsa/service/CurrencyServiceTest.java
@@ -1,44 +1,29 @@
 package sepm.dsa.service;
 
-import com.github.springtestdbunit.DbUnitTestExecutionListener;
-import com.github.springtestdbunit.TransactionDbUnitTestExecutionListener;
-import com.github.springtestdbunit.annotation.DatabaseSetup;
-import junit.framework.TestCase;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestExecutionListeners;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
-import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
 import sepm.dsa.dao.CurrencyAmount;
+import sepm.dsa.dbunit.AbstractDatabaseTest;
 import sepm.dsa.model.Currency;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:testContext.xml"})
-@TestExecutionListeners({
-        DependencyInjectionTestExecutionListener.class,
-        DbUnitTestExecutionListener.class,
-        DirtiesContextTestExecutionListener.class,
-        TransactionDbUnitTestExecutionListener.class
-})
-public class CurrencyServiceTest extends TestCase {
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNull;
+import static org.junit.Assert.assertNotNull;
+
+public class CurrencyServiceTest extends AbstractDatabaseTest {
 
     @Autowired
     private CurrencyService currencyService;
 
     @Test
-    @DatabaseSetup("/testData.xml")
     public void testGet() throws Exception {
         assertNotNull(currencyService.get(1));
     }
 
     @Test
-    @DatabaseSetup("/testData.xml")
     public void testAdd() throws Exception {
         Currency c1 = new Currency();
         c1.setName("fofods");
@@ -50,20 +35,17 @@ public class CurrencyServiceTest extends TestCase {
     }
 
     @Test
-    @DatabaseSetup("/testData.xml")
     public void testRemove() throws Exception {
         currencyService.remove(currencyService.get(1));
         assertNull(currencyService.get(1));
     }
 
     @Test
-    @DatabaseSetup("/testData.xml")
     public void testGetAll() throws Exception {
         assertEquals(currencyService.getAll().size(), 2);
     }
 
     @Test
-    @DatabaseSetup("/testData.xml")
     public void testExchange() throws Exception {
 
         Currency c1 = currencyService.get(1); //to base rate: 3
